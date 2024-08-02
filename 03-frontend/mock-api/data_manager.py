535a183e302f486c51bd4c434f018918f0d3dd36
Francianeny <nirinasoabozy@yahoo.fr>
2024-08-02 23:44:57 +0200

    data_manager.py

--- 03-frontend/mock-api/data_manager.py
+++ 03-frontend/mock-api/data_manager.py
@@ -1,77 +1,88 @@
 import json
 
 class DataManager:
-    def __init__(self, users_file='users.json', places_file='places.json'):
-        self.users_file = users_file
-        self.places_file = places_file
+    def __init__(self, data_file='data.json', user_file='users.json'):
+        self.data_file = data_file
+        self.user_file = user_file
         self.load_data()
+        self.load_users()
 
     def load_data(self):
-        """Load data from JSON files."""
+        """Load data from the JSON file."""
         try:
-            with open(self.users_file, 'r') as file:
-                self.users_data = json.load(file)
+            with open(self.data_file, 'r') as file:
+                self.data = json.load(file)
         except FileNotFoundError:
-            self.users_data = []
+            self.data = {'users': [], 'places': []}
+        except json.JSONDecodeError:
+            self.data = {'users': [], 'places': []}
 
+    def load_users(self):
+        """Load user data from the users JSON file."""
         try:
-            with open(self.places_file, 'r') as file:
-                self.places_data = json.load(file)
+            with open(self.user_file, 'r') as file:
+                self.users = json.load(file)
         except FileNotFoundError:
-            self.places_data = []
+            self.users = []
+        except json.JSONDecodeError:
+            self.users = []
 
     def save_data(self):
-        """Save data to JSON files."""
-        with open(self.users_file, 'w') as file:
-            json.dump(self.users_data, file, indent=4)
+        """Save data to the JSON file."""
+        with open(self.data_file, 'w') as file:
+            json.dump(self.data, file, indent=4)
 
-        with open(self.places_file, 'w') as file:
-            json.dump(self.places_data, file, indent=4)
-
-    # User Methods
     def get_all_users(self):
-        return self.users_data
+        return self.users
 
     def get_user(self, user_id):
-        return next((user for user in self.users_data if user['id'] == user_id), None)
+        return next((user for user in self.users if user['id'] == user_id), None)
 
     def get_user_by_email(self, email):
-        return next((user for user in self.users_data if user['email'] == email), None)
+        return next((user for user in self.users if user['email'] == email), None)
+
+    def verify_user(self, email, password):
+        user = self.get_user_by_email(email)
+        return user if user and user['password'] == password else None
 
     def save_user(self, user_data):
-        user_id = str(len(self.users_data) + 1)  # Simple ID generation logic
-        user_data['id'] = user_id
-        self.users_data.append(user_data)
-        self.save_data()
-        return user_id
+        user_data['id'] = str(len(self.users) + 1)  # Simple ID generation logic
+        self.users.append(user_data)
+        self.save_users()
+        return user_data['id']
+
+    def save_users(self):
+        """Save user data to the users JSON file."""
+        with open(self.user_file, 'w') as file:
+            json.dump(self.users, file, indent=4)
 
     def update_user(self, user_id, updated_data):
         user = self.get_user(user_id)
         if user:
             user.update(updated_data)
-            self.save_data()
+            self.save_users()
             return True
         return False
 
     def delete_user(self, user_id):
         user = self.get_user(user_id)
         if user:
-            self.users_data.remove(user)
-            self.save_data()
+            self.users.remove(user)
+            self.save_users()
             return True
         return False
 
-    # Place Methods
+    # Place Methods (unchanged)
     def get_all_places(self):
-        return self.places_data
+        return self.data['places']
 
     def get_place(self, place_id):
-        return next((place for place in self.places_data if place['id'] == place_id), None)
+        return next((place for place in self.data['places'] if place['place_id'] == place_id), None)
 
     def save_place(self, place_data):
-        place_id = str(len(self.places_data) + 1)  # Simple ID generation logic
-        place_data['id'] = place_id
-        self.places_data.append(place_data)
+        place_id = str(len(self.data['places']) + 1)  # Simple ID generation logic
+        place_data['place_id'] = place_id
+        self.data['places'].append(place_data)
         self.save_data()
         return place_id
 
@@ -86,7 +97,7 @@ class DataManager:
     def delete_place(self, place_id):
         place = self.get_place(place_id)
         if place:
-            self.places_data.remove(place)
+            self.data['places'].remove(place)
             self.save_data()
             return True
         return False
