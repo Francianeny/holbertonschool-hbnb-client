518757388c4c5deaaed4ad5cd6e36e98a5e735e0
Francianeny <nirinasoabozy@yahoo.fr>
2024-08-02 23:21:37 +0200

    app.py

--- 03-frontend/mock-api/app.py
+++ 03-frontend/mock-api/app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+from flask_restx import Api
+from user_api import api as user_api
 from flask import Flask, request, jsonify
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_cors import CORS
