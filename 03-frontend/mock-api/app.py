76d428d7ebe707f3375212ae99f8181a2360255e
Francianeny <nirinasoabozy@yahoo.fr>
2024-08-03 03:17:27 +0200

    app.py

--- 03-frontend/mock-api/app.py
+++ 03-frontend/mock-api/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask_restx import Api
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
